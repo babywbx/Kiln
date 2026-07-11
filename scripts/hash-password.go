a057046708d469b10200a67db31937d8761f07a5
Babywbx <5620530+babywbx@users.noreply.github.com>
2026-07-11 02:04:22 -0400

    🔧 chore(scripts): add password hash and ffmpeg helper examples

---
+++ scripts/hash-password.go
@@ -0,0 +1,23 @@
+//go:build ignore
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stderr, "usage: go run scripts/hash-password.go <password>")
+		os.Exit(2)
+	}
+	h, err := bcrypt.GenerateFromPassword([]byte(os.Args[1]), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	fmt.Println(string(h))
+}
